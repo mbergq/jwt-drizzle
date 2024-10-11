a48691a77da1d8e4e8475af70a56428b791d8697
Martin <martinberg9@proton.me>
2024-10-11 15:20:54 +0200

    successful hashed password

--- src/controllers/createUser.ts
+++ src/controllers/createUser.ts
@@ -1,11 +1,20 @@
 import { type Request, type Response } from "express";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import { user } from "../db/schema.ts";
+import { user } from "../db/schema";
 import { db } from "../index.ts";
 
 const createUser = async (req: Request, res: Response) => {
   const { name, password }: { name: string; password: string } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await db.insert(user).values({ name: name, password: hashedPassword });
+    res.status(201).json({ message: "A new user has been added" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "server_error", message: "Unable to create user" });
+  }
 };
 
 export default createUser;
