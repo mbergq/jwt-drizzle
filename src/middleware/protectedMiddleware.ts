84ee20e7166ea4b7f1f2e6fc5fde690d50ba24f1
Martin <martinberg9@proton.me>
2024-10-17 15:27:58 +0200

    edit cookies name to correct one, remove console logs

--- src/middleware/protectedMiddleware.ts
+++ src/middleware/protectedMiddleware.ts
@@ -7,17 +7,14 @@ dotenv.config({ path: "../../.env" });
 const verifyToken = (req: Request, res: Response, next: NextFunction) => {
   try {
     const token =
-      req.cookies?.createAccessToken ||
+      req.cookies?.accessToken ||
       req.header("Authorization")?.replace("Bearer ", "");
-    console.log("Cookies", req.cookies);
 
     if (!token) return res.status(401).json({ error: "Access denied" });
 
     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!);
-    console.log("Decoded data: ", decoded);
 
     req.body.userId = decoded;
-    console.log(decoded);
     next();
   } catch (error) {
     res.status(401).json({ error: "Invalid token" });
